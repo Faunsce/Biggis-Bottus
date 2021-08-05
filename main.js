16169233124ccb1e782afcda5a531e9e34361bcc
Dominik Rutter <dominik.rutter@gmail.com>
2021-08-05 00:33:18 -0400

    Add opus

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Test");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('token');
